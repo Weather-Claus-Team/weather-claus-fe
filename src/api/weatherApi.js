973fc93b4debc81d87aa8ac94f9ebca26eac114d
HYH <dudgns3864@gmail.com>
2024-10-11 12:45:03 +0900

    Feat : Default-WeatherApi-Add

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -2,15 +2,17 @@ const SERVER_GET_WEATHER_URL = process.env.REACT_APP_SERVER_GET_WEATHER_URL;
 
 const weatherApi = async (city) => {
   try {
-    const response = await fetch(
-      `${SERVER_GET_WEATHER_URL}/api/weather/forecast?city=${city}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    //로그인 구현 후 위,경도에 따른 api 추가 필요
+    const url = city
+      ? `${SERVER_GET_WEATHER_URL}/api/weather/forecast?city=${city}`
+      : `${SERVER_GET_WEATHER_URL}/api/weather`;
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
     if (!response.ok) {
       throw new Error(`Error: ${response.status} ${response.statusText}`);
--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -5,8 +5,8 @@ export const useWeather = (city) => {
   return useQuery({
     queryKey: ["weather", city],
     queryFn: () => weatherApi(city),
-    retry: 1, // 실패한 요청을 재시도하는 횟수 설정
-    refetchOnWindowFocus: true, // 윈도우 포커스 시 다시 요청
+    retry: 0, // 실패한 요청을 재시도하는 횟수 설정
+    refetchOnWindowFocus: false, // 윈도우 포커스 시 다시 요청
     refetchOnReconnect: true, // 네트워크 재연결 시 다시 요청
     staleTime: 100000,
   });
