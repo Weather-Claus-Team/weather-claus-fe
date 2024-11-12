c6a8c75d60c1452f562f3e240e0c27c0ba8ccd3a
lemonsaltytoo <eunsuk.nh@gmail.com>
2024-11-12 15:24:38 +0900

    Fix: api url for deploy

--- src/api/signupApi.js
+++ src/api/signupApi.js
@@ -3,7 +3,7 @@ const SERVER_URL = process.env.REACT_APP_SERVER_GET_WEATHER_URL;
 // 아이디 중복 검사
 export const checkDuplicateUsername = async (username) => {
   try {
-    const url = `${SERVER_URL}/api/users/username`;
+    const url = `${SERVER_URL}/users/username`;
     const response = await fetch(url, {
       method: "POST",
       headers: {
@@ -32,7 +32,7 @@ export const checkDuplicateUsername = async (username) => {
 // 이메일 인증번호 전송
 export const sendEmail = async (email) => {
   try {
-    const url = `${SERVER_URL}/api/users/email`;
+    const url = `${SERVER_URL}/users/email`;
     const response = await fetch(url, {
       method: "POST",
       headers: {
@@ -56,7 +56,7 @@ export const sendEmail = async (email) => {
 // // 이메일 인증번호 검사
 export const checkEmailCode = async (email, emailCode) => {
   try {
-    const url = `${SERVER_URL}/api/users/email-code`;
+    const url = `${SERVER_URL}/users/email-code`;
     const response = await fetch(url, {
       method: "POST",
       headers: {
@@ -80,7 +80,7 @@ export const checkEmailCode = async (email, emailCode) => {
 // // 회원가입 완료
 export const signupResult = async (data) => {
   try {
-    const response = await fetch(`${SERVER_URL}/api/users`, {
+    const response = await fetch(`${SERVER_URL}/users`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
