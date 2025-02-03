bb7a31ed8da30b2050f32f81e8e496c3b7178b4c
HYH <dudgns3864@gmail.com>
2025-02-03 18:20:13 +0900

    Feat: test

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,47 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import weatherApi from "../api/contentApis/weatherApi";
 
-// 오늘 날짜
-const getTodayString = () => new Date().toISOString().split("T")[0];
-
-export const useWeather = (city: string, location: { lat: number; lon: number }) => {
-  const storedData = localStorage.getItem("weatherData");
-  const parsedData = storedData ? JSON.parse(storedData) : null;
-  const savedDate = parsedData?.savedDate;
-  const today = getTodayString();
-
-  //오늘 날짜와 저장된 날짜 비교
-  const shouldFetch = savedDate !== today;
-
-  const { data, isLoading, isError, isFetching } = useQuery({
+export const useWeather = (city:string, location:{lat:number; lon:number;}) => {
+  return useQuery({
     queryKey: ["weather", city, "lat", location?.lat, "lon", location?.lon],
-    queryFn: async () => {
-      const response = await weatherApi(city, location?.lat, location?.lon);
-      
-      //API 요청 성공 시 localStorage에 저장
-      localStorage.setItem(
-        "weatherData",
-        JSON.stringify({
-          weather: response,
-          savedDate: today,
-        })
-      );
-
-      return response;
-    },
-    enabled: shouldFetch,
-    retry: 0,
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: true,
+    queryFn: () => weatherApi(city, location?.lat, location?.lon),
+    retry: 0, // 실패한 요청을 재시도하는 횟수 설정
+    refetchOnWindowFocus: false, // 윈도우 포커스 시 다시 요청
+    refetchOnReconnect: true, // 네트워크 재연결 시 다시 요청
     staleTime: 100000,
-    gcTime: 100000,
   });
-
-  // localStorage에 저장된 데이터가 있으면 반환, 없으면 API 데이터 반환
-  return {
-    data: parsedData?.weather || data,
-    isLoading: isLoading && !parsedData,
-    isError,
-    isFetching,
-  };
 };
